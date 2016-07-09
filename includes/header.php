98272c9ca9591f454ec50941a389898f58cedfa7
Mike Bostone <mikebostone@gmail.com>
2016-07-09 09:34:30 -0500

    mockups are responsive

--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,13 @@
 		
 		<div class="logo">
 			<i class="fa fa-users"></i>
-			<h1>Contact List app</h1>
+			<h1><a href="#">Contact List app</a></h1>
 		</div>
 
 		<nav>
+			<div class="hamburger-icon">
+				<i class="fa fa-bars"></i>
+			</div>
 			<ul class="main-nav">
 				<li><a href="#">Home</a></li>
 				<li><a href="#">Add New Contact</a></li>
--- scripts/core.js
+++ scripts/core.js
@@ -1,6 +1,14 @@
 // Core Scripts
 $(document).ready(function() {
 
-	
+	// Mobile menu
+	$(document).on('click', 'header nav .hamburger-icon .fa', function() {
+		$('header nav').toggleClass('show-mobile-menu');
+	});
+
+	// Close mobile menu
+	$(document).on('click', 'main', function() {
+		$('header nav').removeClass('show-mobile-menu');
+	});
 
 });
